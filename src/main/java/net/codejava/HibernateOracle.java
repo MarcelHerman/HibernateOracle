3cdd0250be302948d7d8d0df500cc7e792dcdca4
MarcelHerman <herman-marcel@wp.pl>
2023-12-11 22:02:06 +0100

    Singleton init

--- src/main/java/net/codejava/HibernateOracle.java
+++ src/main/java/net/codejava/HibernateOracle.java
@@ -1,5 +1,17 @@
 package net.codejava;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -8,33 +20,14 @@ import org.hibernate.cfg.Configuration;
 public class HibernateOracle {
 
 	public static void main(String[] args) {
-		Configuration config = new Configuration();
 		
-		config.setProperty("hibernate.connection.driver_class","oracle.jdbc.OracleDriver");
-		config.setProperty("hibernate.connection.url","jdbc:oracle:thin:@212.33.90.213:1521:xe");
-		config.setProperty("hibernate.connection.username","SBD_ST_PS10_1");
-		config.setProperty("hibernate.connection.password","kotkiwpiwnicy");
-		config.setProperty("hibernate.dialect","org.hibernate.dialect.Oracle8iDialect");
-		config.setProperty("hibernate.show_sql","true");
+		OracleConnection oc =  OracleConnection.getInstance();
 		
-		config.addAnnotatedClass(Kategorie.class);
-		config.addAnnotatedClass(Stany_Zamowienia.class);
-		config.addAnnotatedClass(Magazyny.class);
-		config.addAnnotatedClass(Producenci.class);
-		config.addAnnotatedClass(Typy_uzytkownika.class);
-		config.addAnnotatedClass(Uzytkownicy.class);
-		config.addAnnotatedClass(Zamowienia.class);
-		config.addAnnotatedClass(Produkt_Zamowienia.class);
-		config.addAnnotatedClass(Produkty.class);
-		config.addAnnotatedClass(Produkt_Magazyn.class);
-		config.addAnnotatedClass(Faktury.class);
+		oc.createDBSession();
 		
+		Session session = oc.getDBSession();
 		
-		SessionFactory sessionFactory = config.buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		Transaction transaction = session.beginTransaction();
-		
-		//session.save(new Kategorie("Komputery"));
+		//session.save(new Kategorie("laptopy"));
 		//session.save(new Zamowienia(15.25,"Wiej","Hallera",1,3));
 		//session.save(new Producenci("Biedronka","238989","Bialystok","Pogodna"));
 		//session.save(new Produkty("wiadro 2000",52.32,"jeszcze fajniejsze",1,1));
@@ -43,9 +36,73 @@ public class HibernateOracle {
 		//session.save(new Produkt_Magazyn(new Produkt_Magazyn_Id(1,3),2,2));
 		//session.save(new Faktury("05-10-23","232312",3));
 		
-		transaction.commit();
-		session.close();
-		sessionFactory.close();
+        oc.closeDBSession();
+
+		try
+		{
+			session = oc.getDBSession();
+		}catch(Exception e)
+		{
+            System.out.println("Blad dodania tablicy");
+		}
+		
+		String placeholderLogin = "login";
+		String placeholderPassword = "1234";
+		
+		final JFrame frame = new JFrame("Elektryka Prad Nie Tyka");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        JMenuBar bar = new JMenuBar();
+		JButton zaloguj = new JButton("Zaloguj sie");
+		JButton dodajProduktPrzycisk = new JButton("Produkty");
+		
+		bar.add(dodajProduktPrzycisk);
+		bar.add(Box.createHorizontalGlue());
+		bar.add(zaloguj);
+		
+		 zaloguj.addActionListener(new ActionListener() {
+
+	            public void actionPerformed(ActionEvent e) {
+	            	JTextField loginField = new JTextField(5);
+	                JTextField hasloField = new JTextField(5);
+
+	                JPanel myPanel = new JPanel();
+	                myPanel.add(new JLabel("Login:"));
+	                myPanel.add(loginField);
+	                myPanel.add(Box.createHorizontalStrut(15));
+	                myPanel.add(new JLabel("Haslo:"));
+	                myPanel.add(hasloField);
+
+	                int result = JOptionPane.showConfirmDialog(null, myPanel, 
+	                         "Podaj login i haslo", JOptionPane.OK_CANCEL_OPTION);
+
+	                
+	                try {
+	                	if (result == JOptionPane.OK_OPTION) {
+		                	if(loginField.getText().equals(placeholderLogin ) && hasloField.getText().equals(placeholderPassword)) {
+		                		System.out.println("zalogowano");
+		                		zaloguj.setVisible(false);
+		                		bar.remove(zaloguj);
+		                		
+		                		bar.add(new JLabel(placeholderLogin));
+		                	}
+		                	else
+		                	{
+		                		System.out.println("nie zalogowano");
+		                	}
+		                }
+	                    
+	                } catch (Exception ex) {
+	                    JOptionPane.showMessageDialog(null, "Jakis Blad.");
+	                }
+	                ;
+	            }
+	        });
+		
+		frame.setJMenuBar(bar);
+		
+		frame.setSize(600, 450);
+        frame.setVisible(true);
 		
 	}
 
---
+++ src/main/java/net/codejava/OracleConnection.java
@@ -0,0 +1,68 @@
+package net.codejava;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class OracleConnection {
+	private static OracleConnection instance = null;
+	private static Configuration config;
+	private static SessionFactory sessionFactory;
+	private static Session session;
+	private static Transaction transaction;
+	
+	private OracleConnection() {}
+	
+	public static OracleConnection getInstance() 
+	{
+		if(instance==null)
+		{
+			synchronized(OracleConnection.class)
+			{
+				if(instance==null) instance = new OracleConnection();
+			}		
+		}			
+		return instance;
+	}
+	
+	public void createDBSession()
+	{
+		config = new Configuration();
+		
+		config.setProperty("hibernate.connection.driver_class","oracle.jdbc.OracleDriver");
+		config.setProperty("hibernate.connection.url","jdbc:oracle:thin:@212.33.90.213:1521:xe");
+		config.setProperty("hibernate.connection.username","SBD_ST_PS10_1");
+		config.setProperty("hibernate.connection.password","kotkiwpiwnicy");
+		config.setProperty("hibernate.dialect","org.hibernate.dialect.Oracle8iDialect");
+		config.setProperty("hibernate.show_sql","true");
+		
+		config.addAnnotatedClass(Kategorie.class);
+		config.addAnnotatedClass(Stany_Zamowienia.class);
+		config.addAnnotatedClass(Magazyny.class);
+		config.addAnnotatedClass(Producenci.class);
+		config.addAnnotatedClass(Typy_uzytkownika.class);
+		config.addAnnotatedClass(Uzytkownicy.class);
+		config.addAnnotatedClass(Zamowienia.class);
+		config.addAnnotatedClass(Produkt_Zamowienia.class);
+		config.addAnnotatedClass(Produkty.class);
+		config.addAnnotatedClass(Produkt_Magazyn.class);
+		config.addAnnotatedClass(Faktury.class);
+	}
+	
+	public Session getDBSession()
+	{
+		sessionFactory = config.buildSessionFactory();
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+		
+		return session;
+	}
+	
+	public void closeDBSession()
+	{
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+	}
+}
