9f4694ae0cdd86c3a5943f624a88b3b5340181b2
Choszel <106930061+Choszel@users.noreply.github.com>
2023-12-28 20:23:54 +0100

    maybe dekorator

--- src/main/java/net/codejava/Zamowienia.java
+++ src/main/java/net/codejava/Zamowienia.java
@@ -8,13 +8,58 @@ import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.GenericGenerator;
 
+interface IZamowienia extends Obiekt_Do_Polecen
+{
+	 public String getOpis();
+	 public double getKoszt();
+}
+
+interface IZamowieniaI extends IZamowienia
+{
+	public int getId_zamowienia();
+
+	public void setId_zamowienia(int id_zamowienia);
+
+	public double getKoszt();
+
+	public void setKoszt(double koszt);
+
+	public String getAdres_wysylki_miasto();
+	
+	public void setAdres_wysylki_miasto(String adres_wysylki_miasto);
+
+	public String getAdres_wysylki_ulica();
+
+	public void setAdres_wysylki_ulica(String adres_wysylki_ulica);
+
+	public int getId_stanu_zamowienia();
+
+	public void setId_stanu_zamowienia(int id_stanu_zamowienia);
+
+	public int getUzytkownicy_id_uzytkownika();
+
+	public void setUzytkownicy_id_uzytkownika(int uzytkownicy_id_uzytkownika);
+	
+	public void setOpis(String opis);
+}
+
+
 @Entity
-public class Zamowienia implements Obiekt_Do_Polecen {
+public class Zamowienia implements IZamowieniaI {
 	private int id_zamowienia;
 	private double koszt;
 	private String adres_wysylki_miasto;
 	private String adres_wysylki_ulica;
+	private String opis;
 	
+	public String getOpis() {
+		return opis;
+	}
+
+	public void setOpis(String opis) {
+		this.opis = opis;
+	}
+
 	@ManyToOne
 	@JoinColumn(name = "id_stanu_zamowienia")
 	private int id_stanu_zamowienia;
@@ -84,5 +129,224 @@ public class Zamowienia implements Obiekt_Do_Polecen {
 	public void setUzytkownicy_id_uzytkownika(int uzytkownicy_id_uzytkownika) {
 		this.uzytkownicy_id_uzytkownika = uzytkownicy_id_uzytkownika;
 	}
-
 }
+
+abstract class ZamowieniaDekorator implements IZamowieniaI
+{
+	IZamowieniaI zamowienie;
+	double cena;
+	String opis;
+	
+	public ZamowieniaDekorator(IZamowieniaI zamowienie)
+	{
+		this.zamowienie = zamowienie;
+	}
+	
+	public double getKoszt()
+	{
+		return zamowienie.getKoszt()+cena;
+	}
+	
+	public String getOpis()
+	{
+		return zamowienie.getOpis() + opis;
+	}
+}
+
+class Znizka extends ZamowieniaDekorator
+{
+
+	public Znizka(IZamowieniaI zamowienie, double cena, String opis) {
+		super(zamowienie);
+		this.cena = cena;
+		this.opis = opis;				
+	}
+
+	@Override
+	public int getId_zamowienia() {
+		return 0;
+	}
+
+	@Override
+	public void setId_zamowienia(int id_zamowienia) {		
+	}
+
+	@Override
+	public void setKoszt(double koszt) {		
+	}
+
+	@Override
+	public String getAdres_wysylki_miasto() {
+		return null;
+	}
+
+	@Override
+	public void setAdres_wysylki_miasto(String adres_wysylki_miasto) {		
+	}
+
+	@Override
+	public String getAdres_wysylki_ulica() {
+		return null;
+	}
+
+	@Override
+	public void setAdres_wysylki_ulica(String adres_wysylki_ulica) {		
+	}
+
+	@Override
+	public int getId_stanu_zamowienia() {
+		return 0;
+	}
+
+	@Override
+	public void setId_stanu_zamowienia(int id_stanu_zamowienia) {		
+	}
+
+	@Override
+	public int getUzytkownicy_id_uzytkownika() {
+		return 0;
+	}
+
+	@Override
+	public void setUzytkownicy_id_uzytkownika(int uzytkownicy_id_uzytkownika) {		
+	}
+
+	@Override
+	public void setOpis(String opis) {	
+	}
+	
+}
+
+class Notatka extends ZamowieniaDekorator
+{
+
+	public Notatka(IZamowieniaI zamowienie, double cena, String opis) {
+		super(zamowienie);
+		this.cena = 2.5;
+		this.opis = opis;				
+	}
+
+	@Override
+	public int getId_zamowienia() {
+		return 0;
+	}
+
+	@Override
+	public void setId_zamowienia(int id_zamowienia) {		
+	}
+
+	@Override
+	public void setKoszt(double koszt) {		
+	}
+
+	@Override
+	public String getAdres_wysylki_miasto() {
+		return null;
+	}
+
+	@Override
+	public void setAdres_wysylki_miasto(String adres_wysylki_miasto) {		
+	}
+
+	@Override
+	public String getAdres_wysylki_ulica() {
+		return null;
+	}
+
+	@Override
+	public void setAdres_wysylki_ulica(String adres_wysylki_ulica) {		
+	}
+
+	@Override
+	public int getId_stanu_zamowienia() {
+		return 0;
+	}
+
+	@Override
+	public void setId_stanu_zamowienia(int id_stanu_zamowienia) {		
+	}
+
+	@Override
+	public int getUzytkownicy_id_uzytkownika() {
+		return 0;
+	}
+
+	@Override
+	public void setUzytkownicy_id_uzytkownika(int uzytkownicy_id_uzytkownika) {		
+	}
+
+	@Override
+	public void setOpis(String opis) {	
+	}
+	
+}
+
+class Opakowanie extends ZamowieniaDekorator
+{
+
+	public Opakowanie(IZamowieniaI zamowienie, double cena, String opis) {
+		super(zamowienie);
+		this.cena = 5;
+		this.opis = opis;				
+	}
+
+	@Override
+	public int getId_zamowienia() {
+		return 0;
+	}
+
+	@Override
+	public void setId_zamowienia(int id_zamowienia) {		
+	}
+
+	@Override
+	public void setKoszt(double koszt) {		
+	}
+
+	@Override
+	public String getAdres_wysylki_miasto() {
+		return null;
+	}
+
+	@Override
+	public void setAdres_wysylki_miasto(String adres_wysylki_miasto) {		
+	}
+
+	@Override
+	public String getAdres_wysylki_ulica() {
+		return null;
+	}
+
+	@Override
+	public void setAdres_wysylki_ulica(String adres_wysylki_ulica) {		
+	}
+
+	@Override
+	public int getId_stanu_zamowienia() {
+		return 0;
+	}
+
+	@Override
+	public void setId_stanu_zamowienia(int id_stanu_zamowienia) {		
+	}
+
+	@Override
+	public int getUzytkownicy_id_uzytkownika() {
+		return 0;
+	}
+
+	@Override
+	public void setUzytkownicy_id_uzytkownika(int uzytkownicy_id_uzytkownika) {		
+	}
+
+	@Override
+	public void setOpis(String opis) {	
+	}
+	
+}
+
+
+
+
+
+
