c2a296b482381d80dfe960044d70e5f7e687f7c6
MarcelHerman <herman-marcel@wp.pl>
2023-12-31 15:43:55 +0100

    Interfejs strategii update

--- src/main/java/net/codejava/IStrategia.java
+++ src/main/java/net/codejava/IStrategia.java
@@ -1,10 +1,12 @@
 package net.codejava;
 
+import javax.swing.JPanel;
 import javax.swing.JTable;
 
 import net.codejava.BudowniczyTabeliSwing.ButtonEditor;
 
 public interface IStrategia {
 	public void dodajLogikeEdytowania(ButtonEditor bt);
-	public void dodajLogikeUsuwania();
+	public void dodajLogikeUsuwania(ButtonEditor bt);
+	public void dodajLogikeDodawania(JPanel kontener);
 }
