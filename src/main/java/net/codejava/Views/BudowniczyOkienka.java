cf312b4e65db198b9068ca139496af4c22e93e13
Choszel <106930061+Choszel@users.noreply.github.com>
2024-01-15 23:14:19 +0100

    dfd

--- src/main/java/net/codejava/Views/BudowniczyOkienka.java
+++ src/main/java/net/codejava/Views/BudowniczyOkienka.java
@@ -1,6 +1,11 @@
 package net.codejava.Views;
 
+import javax.swing.Box;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class BudowniczyOkienka {
 
@@ -13,16 +18,23 @@ public class BudowniczyOkienka {
 	
 	public void dodajLabel(String tekst)
 	{
-		
+		JTextField poleTekstowe = new JTextField(7);
+		panel.add(new JLabel(tekst));
+		panel.add(poleTekstowe);	
+		panel.add(Box.createHorizontalStrut(5));
 	}
 	
 	public void dodajCheckBox(String tekst)
 	{
-		
+		JCheckBox checkBox = new JCheckBox(tekst);
+		panel.add(checkBox);
+		panel.add(Box.createHorizontalStrut(5));
 	}
 	
 	public void dodajJComboBox(String[] nazwy)
 	{
-		
+		JComboBox comboBox = new JComboBox(nazwy);
+		panel.add(comboBox);
+		panel.add(Box.createHorizontalStrut(5));
 	}
 }
